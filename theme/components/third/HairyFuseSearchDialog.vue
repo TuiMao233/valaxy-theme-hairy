f03e3045fa1119ae7299f2936803da66c04d6f94
Mr.Mao <951416545@qq.com>
2024-08-27 07:14:44 +0800

    feat: search model animation and auto focus

--- theme/components/third/HairyFuseSearchDialog.vue
+++ theme/components/third/HairyFuseSearchDialog.vue
@@ -20,13 +20,25 @@ whenever(() => !visible.value, () => {
 </script>
 
 <template>
-  <div v-if="visible" role="button" class="DocSearch DocSearch-Container " @click="visible = false">
-    <div class="DocSearch-Modal" style="--docsearch-vh: 5.5200000000000005px;" @click.stop>
-      <HairyFuseSearchHeader v-model:text="text" v-model:visible="visible" />
-      <HairyFuseSearchDropdown v-model:visible="visible" :text="text" />
-      <HairyFuseSearchFooter />
+  <Transition>
+    <div v-if="visible" role="button" class="DocSearch DocSearch-Container " @click="visible = false">
+      <div class="DocSearch-Modal" style="--docsearch-vh: 5.5200000000000005px;" @click.stop>
+        <HairyFuseSearchHeader v-model:text="text" v-model:visible="visible" />
+        <HairyFuseSearchDropdown v-model:visible="visible" :text="text" />
+        <HairyFuseSearchFooter />
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
